9391e5ed8a8a5b8e8bfbfbc6e6e5a258a436d95f
Serge1sm1rn <serejaz.00@mail.ru>
2022-08-12 18:23:38 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MyTankDZ
+
+Developed with Unreal Engine 4
---
+++ Source/MyTankDZ/MyTankDZGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyTankDZGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTANKDZ_API AMyTankDZGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
