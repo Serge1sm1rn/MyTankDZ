d0a342f29d150c8db9156cd689f8b815e79ff57c
Serge1sm1rn <serejaz.00@mail.ru>
2022-08-16 22:40:26 +0300

    Grenade Launcher Create

--- Source/MyTankDZ/MyTank.cpp
+++ Source/MyTankDZ/MyTank.cpp
@@ -170,10 +170,10 @@ void AMyTank::Tick(float DeltaTime)
 		CannonAttachment->GetComponentLocation() + CannonAttachment->GetForwardVector() * 100, FColor::Green,
 		false,-1,0,5);
 
-	//	DrawDebugLine(GetWorld(),
-		//GrenadeLauncherAttachment->GetComponentLocation(),
-		//GrenadeLauncherAttachment->GetComponentLocation() + GrenadeLauncherAttachment->GetForwardVector() * 100, FColor::Green,
-		//false,-1,0,5);
+	    DrawDebugLine(GetWorld(),
+		GrenadeLauncherAttachment->GetComponentLocation(),
+		GrenadeLauncherAttachment->GetComponentLocation() + GrenadeLauncherAttachment->GetForwardVector() * 1000, FColor::Yellow,
+		false,-1,0,5);
 	}
 	
 }
